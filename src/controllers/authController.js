c3c24111ade7dce4de8c14287c55554344924916
Connor <connormorrissey370@gmail.com>
2025-01-12 10:18:18 -0600

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,38 +5,11 @@ const jwt = require('jsonwebtoken');
 exports.register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
-        
-        let user = await User.findOne({ $or: [{ email }, { username }] });
-        if (user) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        user = new User({
-            username,
-            email,
-            password: hashedPassword
-        });
-
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
         await user.save();
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            }
-        );
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(201).json({ token });
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
@@ -45,32 +18,14 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
-
         const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+        
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            }
-        );
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.json({ token });
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
@@ -78,7 +33,7 @@ exports.login = async (req, res) => {
 
 exports.getMe = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findById(req.user.userId).select('-password');
         res.json(user);
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
@@ -87,10 +42,9 @@ exports.getMe = async (req, res) => {
 
 exports.updateProfile = async (req, res) => {
     try {
-        const { username, email } = req.body;
         const user = await User.findByIdAndUpdate(
-            req.user.id,
-            { username, email },
+            req.user.userId,
+            { $set: req.body },
             { new: true }
         ).select('-password');
         res.json(user);
